1144a024476b01563eb820c45ddb4a0a989ca2d2
annakvach <70780985+annakvach@users.noreply.github.com>
2023-08-15 22:14:21 +0300

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -14,12 +14,8 @@ df['combined'] = df[target_cols].apply(lambda row: '/'.join(row.values.astype(st
 # leave only useful columns
 df1 = df[["SNP Name","Sample ID", "combined"]]
 
-# unmelt df1
+# unmelt df1 to get "SNP Name" as cols, "Sample ID" as rows
 df_unmelted = df1.pivot(index='Sample ID', columns='SNP Name', values='combined')
-
-# # view
-# print(df.head())
-# print(df_unmelted.head(n=23))
 
 # save data as json
 df_unmelted.to_json(path_or_buf="BigData/Result_Table.json")
